9c08eecab1afa00afe1aeafd085e71575626aa0a
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2017-08-12 03:19:53 -0300

    Moves oh-my-zsh to zsh folder and includes zsh plugins symlink creation to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 USER=$(whoami)
 HOME=/home/$USER
-DOTFILES=/$HOME/.dotfiles
+DOTFILES=$HOME/.dotfiles
 
 echo "Running install scripts\n\n"
 
@@ -11,7 +11,12 @@ sudo $DOTFILES/tmux/install.sh
 
 echo "Creating symlinks\n\n"
 
-rm $HOME/.zshrc && ln -s $DOTFILES/zsh/conf $HOME/.zshrc
-rm $HOME/.vimrc && ln -s $DOTFILES/vim/conf $HOME/.vimrc
-rm $HOME/.tmux.conf && ln -s $DOTFILES/tmux/conf $HOME/.tmux.conf
+rm -rf $HOME/.zshrc && ln -s $DOTFILES/zsh/conf $HOME/.zshrc
+rm -rf $HOME/.vimrc && ln -s $DOTFILES/vim/conf $HOME/.vimrc
+rm -rf $HOME/.tmux.conf && ln -s $DOTFILES/tmux/conf $HOME/.tmux.conf
 
+echo "Installing zsh plugins"
+
+for i in $(ls $DOTFILES/zsh/oh-my-zsh/plugins); do
+  rm -rf $ZSH/plugins/$i && ln -s $DOTFILES/zsh/oh-my-zsh/plugins/$i $ZSH/plugins/
+done
