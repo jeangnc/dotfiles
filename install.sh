5a30d71f247bfce00aceb6ed709d988178c3e774
Jean Gonçalves <jean.gnc@gmail.com>
2021-03-29 10:28:40 -0300

    Force symlink creation

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 DOTFILES=`realpath $(dirname "$0")`
 
 echo "Creating symlinks"
+ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
+ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -sf $DOTFILES/tmux/tmuxconf $HOME/.tmux.conf
+ln -sf $DOTFILES/system/bashrc $HOME/.bashrc
+ln -sf $DOTFILES/system/aliases $HOME/.aliases
+ln -sf $DOTFILES/system/profile $HOME/.profile
 
 rm -rf $HOME/.zshrc && ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
 rm -rf $HOME/.vimrc && ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
@@ -20,4 +26,4 @@ fc-cache -fv $HOME/.fonts
 
 echo  "Setting up vim filetype plugin"
 
-rm -rf $HOME/.vim/ftplugin && ln -s $DOTFILES/vim/languages $HOME/.vim/ftplugin
+ln -sf $DOTFILES/vim/languages $HOME/.vim/ftplugin
