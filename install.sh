1b9ac36986d0ccafc1616bfcb2c51b3bc9f19485
Jean Gonçalves <jean.gnc@gmail.com>
2021-03-29 10:33:50 -0300

    Improve install script organization

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 DOTFILES=`realpath $(dirname "$0")`
 
-echo "Creating symlinks"
+# symlinks
 ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
 ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
 ln -sf $DOTFILES/tmux/tmuxconf $HOME/.tmux.conf
@@ -8,18 +8,17 @@ ln -sf $DOTFILES/system/bashrc $HOME/.bashrc
 ln -sf $DOTFILES/system/aliases $HOME/.aliases
 ln -sf $DOTFILES/system/profile $HOME/.profile
 
+rm -rf $HOME/.vim/ftplugin && ln -s $DOTFILES/vim/languages $HOME/.vim/ftplugin
+
 touch $HOME/.aliases.local
 touch $HOME/.profile.local
 
 # git
 rm -rf $HOME/.gitignore_global && ln -s $DOTFILES/git/gitignore_global $HOME/.gitignore_global
 
-echo "Installing fonts"
+# fonts
 mkdir -p $HOME/.fonts/{truetype,opentype}/
 cp $DOTFILES/fonts/*.ttf $HOME/.fonts/truetype/ 2>/dev/null
 cp $DOTFILES/fonts/*.otf $HOME/.fonts/opentype/ 2>/dev/null
 fc-cache -fv $HOME/.fonts
 
-echo  "Setting up vim filetype plugin"
-
-rm -rf $HOME/.vim/ftplugin && ln -s $DOTFILES/vim/languages $HOME/.vim/ftplugin
