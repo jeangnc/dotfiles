de6697ad8bac4fa1f03792379fdca09cd7f55756
Jean Gonçalves <jean.gnc@gmail.com>
2021-03-29 10:28:52 -0300

    Create useful local files

--- install.sh
+++ install.sh
@@ -8,12 +8,8 @@ ln -sf $DOTFILES/system/bashrc $HOME/.bashrc
 ln -sf $DOTFILES/system/aliases $HOME/.aliases
 ln -sf $DOTFILES/system/profile $HOME/.profile
 
-rm -rf $HOME/.zshrc && ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
-rm -rf $HOME/.vimrc && ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
-rm -rf $HOME/.tmux.conf && ln -s $DOTFILES/tmux/tmuxconf $HOME/.tmux.conf
-rm -rf $HOME/.bashrc && ln -s $DOTFILES/system/bashrc $HOME/.bashrc
-rm -rf $HOME/.aliases && ln -s $DOTFILES/system/aliases $HOME/.aliases
-rm -rf $HOME/.profile && ln -s $DOTFILES/system/profile $HOME/.profile
+touch $HOME/.aliases.local
+touch $HOME/.profile.local
 
 # git
 rm -rf $HOME/.gitignore_global && ln -s $DOTFILES/git/gitignore_global $HOME/.gitignore_global
