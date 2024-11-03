96440cb0b2f950a415aab76c08a7f5f2886321eb
Jean Gonçalves <jeangnc@Mac.lan>
2024-11-02 22:44:01 -0400

    UI improvements

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -6,4 +6,30 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        indent = {
+          padding = 0, -- Reduz o espaço ao redor dos ícones
+        },
+        git_status = {
+          symbols = {
+            unstaged = "",
+            staged = "",
+          },
+        },
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        show_buffer_icons = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
 }
