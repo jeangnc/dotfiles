2ba923d4dc06b28c7d0b49f59618c860a9a01e76
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-04 21:00:02 -0500

    Refactories the config to be more idiomatic

--- nvim/lua/plugins/ruby.lua
+++ nvim/lua/plugins/ruby.lua
@@ -1,24 +1,30 @@
 vim.g.lazyvim_ruby_lsp = "solargraph"
 vim.g.lazyvim_ruby_formatter = "rubocop"
 
-local nvim_lsp = require("lspconfig")
-
-nvim_lsp.solargraph.setup({
-  filetypes = { "ruby", "rakefile" },
-  root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
-  settings = {
-    solargraph = {
-      autoformat = true,
-      completion = true,
-      diagnostic = true,
-      folding = true,
-      references = true,
-      rename = true,
-      symbols = true,
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        solargraph = {
+          filetypes = { "ruby", "rakefile" },
+          root_dir = function(fname)
+            return require("lspconfig").util.root_pattern("Gemfile", ".git", ".")(fname)
+          end,
+          settings = {
+            solargraph = {
+              autoformat = true,
+              completion = true,
+              diagnostic = true,
+              folding = true,
+              references = true,
+              rename = true,
+              symbols = true,
+            },
+          },
+        },
+      },
     },
   },
-})
-
-return {
   { import = "lazyvim.plugins.extras.lang.ruby" },
 }
