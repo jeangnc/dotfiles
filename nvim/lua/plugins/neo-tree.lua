56ae1c65c423e1cc3d41da3b93e4f05b5b4fe187
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-05 11:47:00 -0500

    Close file browser after opening a file

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,14 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
       default_component_configs = {
         indent = {
           padding = 0, -- Reduz o espaço ao redor dos ícones
