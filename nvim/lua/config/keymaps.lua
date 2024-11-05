033d090ad89ea90920861fecb2d5e83eed34b6bb
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-05 09:53:04 -0500

    Describes keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- copy to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy the whole line to clipboard" })
 
--- open nvim configs in a new tab
-vim.keymap.set("n", "<leader><tab>v", "<cmd>tabnew ~/.config/nvim<cr>", { silent = true, noremap = true })
+vim.keymap.set(
+  "n",
+  "<leader><tab>v",
+  "<cmd>tabnew ~/.config/nvim<cr>",
+  { desc = "Opens nvim configs for editing", silent = true, noremap = true }
+)
