01fc87c00b79d4892e75d1c03f0d3f83d5aa956a
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-10 12:20:50 -0300

    Changes configs

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,16 +8,15 @@ return {
       bigfile = { enabled = true },
       notifier = {
         enabled = true,
-        style = "minimal",
         top_down = false,
-        -- timeout = 5000,
+        timeout = 3000,
       },
       quickfile = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
       styles = {
         notification = {
-          wo = { wrap = true }, -- Wrap notifications
+          wo = { wrap = false }, -- Wrap notifications
         },
       },
     },
