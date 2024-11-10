66b8d708f1151e9c25dfe7e118bcba407a787a25
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-10 16:10:08 -0300

    Setup cmp navigation with C-j and C-k

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -10,6 +10,8 @@ return {
       ["<CR>"] = cmp.config.disable,
       ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
     })
   end,
 }
