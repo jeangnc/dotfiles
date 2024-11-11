1ed94513332694b1d8c66e2ef305a906bd8e7ed0
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-11 11:58:43 -0300

    Fixes neo-tree navigation

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,24 +1,27 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      event_handlers = {
-        {
-          event = "file_open_requested",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
+  "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
+  opts = {
+    filesystem = {
+      bind_to_cwd = true,
+      hijack_netrw_behavior = "open_current",
+    },
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          require("neo-tree.command").execute({ action = "close" })
+        end,
       },
-      default_component_configs = {
-        indent = {
-          padding = 0, -- Reduz o espaço ao redor dos ícones
-        },
-        git_status = {
-          symbols = {
-            unstaged = "",
-            staged = "",
-          },
+    },
+    default_component_configs = {
+      indent = {
+        padding = 0, -- Reduz o espaço ao redor dos ícones
+      },
+      git_status = {
+        symbols = {
+          unstaged = "",
+          staged = "",
         },
       },
     },
