40768da9c8e71d37cce6c0d5e61bd1a2ea3906cc
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-11 18:05:24 -0300

    Removes unused keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy the whole line to clipboard" })
-
 vim.keymap.set(
   "n",
   "<leader><tab>v",
