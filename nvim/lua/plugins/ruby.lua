281d5bc20d0fc6a7d4a4a86e20bc6ef7e94f8641
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-11 09:08:51 -0300

    Temporarily revert to solargraph

--- nvim/lua/plugins/ruby.lua
+++ nvim/lua/plugins/ruby.lua
@@ -1,44 +1,39 @@
+vim.g.lazyvim_ruby_lsp = "solargraph"
+vim.g.lazyvim_ruby_formatter = "rubocop"
+
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = { "ruby" },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = { "erb-formatter", "erb-lint" },
-    },
-  },
+  -- this will setup some helpful configs
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+
   -- sets up LSP
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         -- sorbet for better typechecking
-        sorbet = {
-          filetypes = { "ruby", "rakefile" },
-          cmd = { "srb", "tc", "--lsp", "--typed=true", "--no-config" },
-          settings = {},
-          init_options = {
-            formatting = false,
-          },
-        },
+        -- sorbet = {
+        --   filetypes = { "ruby", "rakefile" },
+        --   cmd = { "srb", "tc", "--lsp", "--typed=true", "--no-config" },
+        --   settings = {},
+        --   init_options = {
+        --     formatting = false,
+        --   },
+        -- },
         -- solargraph for linting
-        solargraph = {
-          enabled = true,
-          filetypes = { "ruby", "rakefile" },
-          settings = {
-            solargraph = {
-              diagnostics = true,
-              definitions = false,
-            },
-          },
-        },
+        -- solargraph = {
+        --   enabled = true,
+        --   filetypes = { "ruby", "rakefile" },
+        --   settings = {
+        --     solargraph = {
+        --       diagnostics = true,
+        --       definitions = false,
+        --     },
+        --   },
+        -- },
       },
     },
   },
 
+  -- other plugins
   { "tpope/vim-rails" },
 }
