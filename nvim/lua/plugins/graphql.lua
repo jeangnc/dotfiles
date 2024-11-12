1855f07928525bc3de6f22c9c8de03836b9de8fe
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-12 15:21:38 -0300

    Setup graphql

---
+++ nvim/lua/plugins/graphql.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "graphql-language-service-cli",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        "graphql",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "graphql",
+      },
+    },
+  },
+}
