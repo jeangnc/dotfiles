a3d851bf60aa1239f4a105125d2a02788e9423cd
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-13 15:17:06 -0300

    Solves telescope open in tab keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,17 +1,20 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
+  opts = function(_, opts)
+    local actions = require("telescope.actions")
+    opts.defaults = {
       mappings = {
         i = {
-          ["<C-j>"] = require("telescope.actions").move_selection_next, -- Move para o próximo item
-          ["<C-k>"] = require("telescope.actions").move_selection_previous, -- Move para o item anterior
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          ["<c-t>"] = actions.select_tab,
         },
         n = {
-          ["<C-j>"] = require("telescope.actions").move_selection_next, -- Move para o próximo item no modo normal
-          ["<C-k>"] = require("telescope.actions").move_selection_previous, -- Move para o item anterior no modo normal
+          ["<C-j>"] = require("telescope.actions").move_selection_next,
+          ["<C-k>"] = require("telescope.actions").move_selection_previous,
+          ["<c-t>"] = actions.select_tab,
         },
       },
-    },
-  },
+    }
+  end,
 }
