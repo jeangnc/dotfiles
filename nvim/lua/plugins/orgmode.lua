5105c13d98fddab8a7f92dcd3e5ea14ae8a9fa58
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-14 18:13:13 -0300

    Best orgmode keymaps

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -1,15 +1,46 @@
 -- required to compile the norg treesitter parser
 require("nvim-treesitter.install").compilers = { "gcc-14" }
 
-vim.keymap.set("n", "<leader>of", function()
-  open_context("~/.orgfiles")
-end, { desc = "Opens orgfiles directory", silent = true, noremap = true })
+vim.keymap.set(
+  "n",
+  "<leader>oe",
+  "<cmd>Telescope neorg find_norg_files<cr>",
+  { desc = "Explore orgfiles", silent = true, noremap = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ojy",
+  "<cmd>Neorg journal yesterday<cr>",
+  { desc = "Open yesterday's journal", silent = true, noremap = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ojt",
+  "<cmd>Neorg journal today<cr>",
+  { desc = "Open today's journal", silent = true, noremap = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>ojn",
+  "<cmd>Neorg journal tomorrow<cr>",
+  { desc = "Open tomorrow's journal", silent = true, noremap = true }
+)
 
 vim.keymap.set(
   "n",
   "<leader>oi",
   "<cmd>Neorg index<cr>",
   { desc = "Opens neorg index file", silent = true, noremap = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>owe",
+  "<cmd>Neorg workspace<cr>",
+  { desc = "Explore workspaces", silent = true, noremap = true }
 )
 
 vim.keymap.set(
@@ -103,6 +134,8 @@ return {
       spec = {
         {
           { "<leader>o", group = "orgmode" },
+          { "<leader>ow", group = "workspaces" },
+          { "<leader>oj", group = "journal" },
           { "<localleader>a", group = "Append" },
         },
       },
