0d0b8d155881312af6ec03ce12baf81f3d7cca65
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-20 21:55:07 -0300

    Setup minitest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -4,9 +4,28 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "olimorris/neotest-rspec",
+      "zidhuss/neotest-minitest",
     },
     opts = {
       adapters = {
+        ["neotest-minitest"] = {
+          minitest_cmd = function()
+            return vim.tbl_flatten({
+              "docker-compose",
+              "exec",
+              "web",
+              "bundle",
+              "exec",
+              "rails",
+              "test",
+            })
+          end,
+          transform_spec_path = function(path)
+            local prefix = require("neotest-minitest").root(path)
+            return string.sub(path, string.len(prefix) + 2, -1)
+          end,
+          results_path = "tmp/minitest.output",
+        },
         ["neotest-rspec"] = {
           rspec_cmd = function()
             return vim.tbl_flatten({
