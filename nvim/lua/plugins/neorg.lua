4f5cecaae95e48360380e5a429f84e0a439cbef1
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-24 13:35:48 -0300

    Indents neorg file when I leave insert mode

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -84,6 +84,16 @@ return {
             vim.cmd("startinsert")
             vim.cmd('lua require("neorg.modules.core.itero.module").public.next_iteration_cr()')
           end, { desc = "", buffer = true })
+
+          -- indents the file after leaving insert mode
+          vim.api.nvim_create_autocmd("InsertLeave", {
+            buffer = 0, -- Limit this autocmd to the current buffer
+            callback = function()
+              local current_pos = vim.api.nvim_win_get_cursor(0) -- Get the current cursor position
+              vim.cmd("silent normal! gg=G") -- Indent the entire file
+              vim.api.nvim_win_set_cursor(0, current_pos) -- Restore the cursor position
+            end,
+          })
         end,
       })
     end,
