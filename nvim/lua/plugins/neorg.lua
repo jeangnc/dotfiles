167d4c08657e75c7d0840a4ccb4194bef9408ccf
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-24 13:24:23 -0300

    Removes comments

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,13 +1,6 @@
 -- required to compile the norg treesitter parser
 require("nvim-treesitter.install").compilers = { "gcc-14" }
 
-local function first_day_of_week(date)
-  local currentDayOfWeek = os.date("*t", date).wday
-  local daysToSubtract = currentDayOfWeek - 1
-  local firstDayOfWeek = os.date("*t", date - (daysToSubtract * 86400))
-  return string.format("%04d-%02d-%02d", firstDayOfWeek.year, firstDayOfWeek.month, firstDayOfWeek.day)
-end
-
 return {
   {
     "nvim-neorg/neorg",
@@ -21,27 +14,6 @@ return {
       { "<leader>ojp", "<cmd>Neorg journal yesterday<cr>", desc = "Open previous day's journal" },
       { "<leader>ojc", "<cmd>Neorg journal today<cr>", desc = "Open current today's journal" },
       { "<leader>ojn", "<cmd>Neorg journal tomorrow<cr>", desc = "Open next day's journal" },
-      -- {
-      --   "<leader>ojwc",
-      --   function()
-      --     vim.cmd("Neorg journal custom " .. first_day_of_week(os.time()))
-      --   end,
-      --   desc = "Open current week's journal",
-      -- },
-      -- {
-      --   "<leader>ojwp",
-      --   function()
-      --     vim.cmd("Neorg journal custom " .. first_day_of_week(os.time() - 7 * 86400))
-      --   end,
-      --   desc = "Open previous week's journal",
-      -- },
-      -- {
-      --   "<leader>ojwn",
-      --   function()
-      --     vim.cmd("Neorg journal custom " .. first_day_of_week(os.time() - 7 * 86400))
-      --   end,
-      --   desc = "Open next week's journal",
-      -- },
     },
     config = function()
       require("neorg").setup({
@@ -136,8 +108,6 @@ return {
           { "<leader>o", group = "orgmode", icon = " " },
           { "<leader>ow", group = "workspaces", icon = " " },
           { "<leader>oj", group = "journal", icon = " " },
-          -- { "<leader>ojd", group = "Day" },
-          -- { "<leader>ojw", group = "Week" },
           { "<localleader>a", group = "Append" },
         },
       },
