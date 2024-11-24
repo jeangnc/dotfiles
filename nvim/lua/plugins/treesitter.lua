e7219f35ae87f915c1bf665eecddbcec67efdc53
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-24 13:59:56 -0300

    Preserves default J behavior

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
   {
     "wansmer/treesj",
     keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+      { "gJ", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
