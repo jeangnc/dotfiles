dc790da7f9126c34869535f88814f6b64aa8e953
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-24 13:26:10 -0300

    Improves join using TS

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,4 +7,11 @@ return {
       },
     },
   },
+  {
+    "wansmer/treesj",
+    keys = {
+      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
 }
