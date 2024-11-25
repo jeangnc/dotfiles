77d660ef5baa71efa1df198167fe358802d35cdb
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-24 21:25:29 -0300

    Removes unnecessary nesting

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,20 +1,17 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = "*",
-    config = function()
-      require("mini.splitjoin").setup()
-      require("mini.surround").setup({
-        mappings = {
-          add = "gsa", -- Add surrounding in Normal and Visual modes
-          delete = "gsd", -- Delete surrounding
-          find = "gsf", -- Find surrounding (to the right)
-          find_left = "gsF", -- Find surrounding (to the left)
-          highlight = "gsh", -- Highlight surrounding
-          replace = "gsr", -- Replace surrounding
-          update_n_lines = "gsn", -- Update `n_lines`
-        },
-      })
-    end,
-  },
+  "echasnovski/mini.nvim",
+  config = function()
+    require("mini.splitjoin").setup()
+    require("mini.surround").setup({
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    })
+  end,
 }
