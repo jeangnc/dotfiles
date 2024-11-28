49a9e4b636c5bc0282bbb3c0f3b5e7b217590a9f
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-28 09:09:19 -0300

    Changes bufferline mode to tabs

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
+        mode = "tabs",
         show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
