6c67a909a0ffc37141a0bf2ee01cb2ee6ddaf6d8
Jean Gonçalves <jean.gnc@gmail.com>
2024-11-28 09:09:34 -0300

    Setup catppuccin integrations

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -1,9 +1,48 @@
 return {
   {
-    "LazyVim/LazyVim",
-    dependencies = {
-      "catppuccin/nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      integrations = {
+        cmp = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
     },
+  },
+  {
+    "LazyVim/LazyVim",
+    dependencies = {},
     opts = {
       colorscheme = "catppuccin-mocha",
       icons = {
