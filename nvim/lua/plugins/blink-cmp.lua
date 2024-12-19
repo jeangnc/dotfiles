bb3eed2103d24497e6d4add40689f0eb3c2173a6
Jean Gonçalves <jean.gnc@gmail.com>
2024-12-19 13:15:43 -0300

    Configures blink keymaps

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,29 @@
+return {
+  "saghen/blink.cmp",
+  lazy = false, -- lazy loading handled internally
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  opts = {
+    keymap = {
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide" },
+      ["<C-l>"] = { "select_and_accept" },
+
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+
+      ["<C-d>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = "manual",
+      },
+    },
+  },
+}
