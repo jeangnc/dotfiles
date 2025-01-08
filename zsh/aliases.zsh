eed4ab4b7e37f9be3502f4c78d534f877129a75a
Jean Gonçalves <jean.gnc@gmail.com>
2025-01-08 07:50:05 -0300

    Rewrites git sync

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,8 @@ alias gps='git push origin "$(git_current_branch)"'
 alias gpl='git pull origin "$(git_current_branch)"'
 alias gbd='function _gbd() { git branch -d "$1" && git push origin --delete "$1"; }; _gbd'
 
-## git sync
-alias gs="git checkout main && git pull && git branch --merged | grep -v '\*' | grep -v 'main' | xargs -n 1 git branch -d"
-
+# "git update"
+alias gu='git checkout -q main && git pull && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
 
 # editor
 alias vi="nvim"
