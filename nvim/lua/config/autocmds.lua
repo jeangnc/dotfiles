84c2eb580fc0a6429106d175dcaeca19d89df8e3
Jean Gonçalves <jean.gnc@gmail.com>
2025-01-26 20:10:13 -0300

    Fix folds not persisting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,34 +2,16 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-local view_group = augroup("auto_view", { clear = true })
-autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
-  desc = "Save view with mkview for real files",
-  group = view_group,
-  callback = function(args)
-    -- save view async
-    if vim.b[args.buf].view_activated then
-      vim.defer_fn(function()
-        vim.cmd.mkview({ mods = { emsg_silent = true } })
-      end, 0)
-    end
+vim.api.nvim_create_autocmd({ "BufWinLeave", "BufWritePost" }, {
+  pattern = { "*" },
+  callback = function(event)
+    vim.cmd.mkview({ mods = { emsg_silent = true } })
   end,
 })
-autocmd("BufWinEnter", {
-  desc = "Try to load file view if available and enable view saving for real files",
-  group = view_group,
-  callback = function(args)
-    if not vim.b[args.buf].view_activated then
-      local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
-      local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
-      local ignore_filetypes = { "gitcommit", "gitrebase", "svg", "hgcommit" }
-      if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
-        vim.b[args.buf].view_activated = true
-        vim.cmd.loadview({ mods = { emsg_silent = true } })
-      end
-    end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*" },
+  callback = function(event)
+    vim.cmd.loadview({ mods = { emsg_silent = true } })
   end,
 })
