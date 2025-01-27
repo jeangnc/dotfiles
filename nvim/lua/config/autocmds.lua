aa67e229846aa96f99012e4e0e50b21753b9b134
Jean Gonçalves <jean.gnc@gmail.com>
2025-01-26 22:31:23 -0300

    Fixes view persistance again

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,16 +2,25 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local group = vim.api.nvim_create_augroup("autoview", { clear = true })
+
+-- creates a view when leading the file
 vim.api.nvim_create_autocmd({ "BufWinLeave", "BufWritePost" }, {
-  pattern = { "*" },
-  callback = function(event)
+  group = group,
+  pattern = "*",
+  callback = function()
     vim.cmd.mkview({ mods = { emsg_silent = true } })
   end,
 })
 
-vim.api.nvim_create_autocmd("BufWinEnter", {
-  pattern = { "*" },
-  callback = function(event)
-    vim.cmd.loadview({ mods = { emsg_silent = true } })
+-- reads view after buffer is readed
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = group,
+  pattern = "*",
+  callback = function()
+    -- defers execution, otherwise doesn't work when the file is opened
+    vim.defer_fn(function()
+      vim.cmd.loadview({ mods = { emsg_silent = true } })
+    end, 0)
   end,
 })
