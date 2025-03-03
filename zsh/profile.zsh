f595d158a84eff3200c10ace5b782c78bd51b1af
Jean Gonçalves <jean.gnc@gmail.com>
2025-03-02 23:48:08 -0300

    Setup cargo

--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -15,3 +15,6 @@ source <(fzf --zsh)
 
 # qlty completions
 [ -s "/opt/homebrew/share/zsh/site-functions/_qlty" ] && source "/opt/homebrew/share/zsh/site-functions/_qlty"
+
+# cargo
+. "$HOME/.cargo/env"
