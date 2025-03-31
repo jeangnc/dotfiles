7dde9218ed7c793bc1351099d2df1052ccf9a2e5
Jean Gonçalves <jean.gnc@gmail.com>
2025-03-31 08:33:02 -0300

    Use copilot provider

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -5,24 +5,20 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
-      max_tokens = 4096,
-      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
-    },
+    provider = "copilot",
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+
+    -- optional
+    "ibhagwan/fzf-lua",
+    "zbirenbaum/copilot.lua",
+    -- "kaiser-Yang/blink-cmp-avante",
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
