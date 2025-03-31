c02da052b2fd8d5ea8de7bdb5b0d71befedcfac2
Jean Gonçalves <jean.gnc@gmail.com>
2025-03-31 08:56:47 -0300

    Use openai model

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -3,9 +3,15 @@ return {
   event = "VeryLazy",
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    -- add any opts here
-    -- for example
-    provider = "copilot",
+    provider = "openai",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      temperature = 0,
+      max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
