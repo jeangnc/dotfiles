bd530142ae84feedbd55291719dd8a4ebce311b3
Jean Gonçalves <jean.gnc@gmail.com>
2025-04-17 22:38:52 -0300

    Increase scratch window size

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,29 +1,59 @@
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      dashboard = { enabled = false },
-      bigfile = { enabled = true },
-      notifier = {
-        enabled = true,
-        top_down = false,
-        timeout = 3000,
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  keys = {
+    {
+      "<leader>.",
+      function()
+        Snacks.scratch()
+      end,
+      desc = "Toggle Scratch Buffer",
+    },
+    {
+      "<leader>S",
+      function()
+        Snacks.scratch.select()
+      end,
+      desc = "Select Scratch Buffer",
+    },
+  },
+  ---@type snacks.Config
+  opts = {
+    dashboard = { enabled = false },
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+
+    notifier = {
+      enabled = true,
+      top_down = false,
+      timeout = 3000,
+    },
+
+    ---@type table<string, snacks.win.Config>
+    styles = {
+      notification = {
+        wo = { wrap = true },
       },
-      quickfile = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
-      styles = {
-        notification = {
-          wo = { wrap = true },
-        },
-      },
-      terminal = {
-        win = {
-          position = "float",
-        },
+      scratch = {
+        width = 200,
+        height = 30,
       },
     },
+
+    terminal = {
+      win = {
+        position = "float",
+      },
+    },
+
+    ---@class snacks.scratch.Config
+    ---@field win? snacks.win.Config scratch window
+    ---@field template? string template for new buffers
+    ---@field file? string scratch file path. You probably don't need to set this.
+    ---@field ft? string|fun():string the filetype of the scratch buffer
+    scratch = {},
   },
 }
