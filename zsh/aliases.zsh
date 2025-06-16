5d55acad1ab83902c80fd27103bfd1a975d8fd2a
Jean Gonçalves <jean.gnc@gmail.com>
2025-06-16 09:24:34 -0300

    Deletes branch with capital D

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # git
 alias gps='git push origin "$(git_current_branch)"'
 alias gpl='git pull origin "$(git_current_branch)"'
-alias gbd='function _gbd() { git branch -d "$1" && git push origin --delete "$1"; }; _gbd'
+alias gbd='function _gbd() { git branch -D "$1" && git push origin --delete "$1"; }; _gbd'
 
 # "git update"
 alias gu='git checkout -q main && git pull && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
