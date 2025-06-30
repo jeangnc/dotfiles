a2a1d243b4743958e3bea9721dc9bcc1053c316c
Jean Gonçalves <jean.gnc@gmail.com>
2025-06-30 06:25:46 -0300

    Adds keymap to search datadog metrics

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -62,3 +62,39 @@ map("n", "<leader>0", LazyVim.pick("live_grep"), { desc = "Grep (Root Dir)" })
 -- yanks to global clipboard
 map({ "n", "v", "o" }, "<leader>y", '"+y', { desc = "Yank to Clipboard", silent = true, noremap = true })
 map("n", "<leader>Y", ":%y+<cr>", { desc = "Yank File to Clipboard", silent = true, noremap = true })
+
+----------------------
+
+-- search datadog metrics
+vim.keymap.set("n", "<leader>dm", function()
+  local fzf = require("fzf-lua")
+
+  local handle = io.popen("zsh -ic 'ddmm'")
+  local result = handle:read("*a")
+  handle:close()
+
+  local lines = {}
+  for line in result:gmatch("[^\r\n]+") do
+    if line ~= "" then
+      table.insert(lines, line)
+    end
+  end
+
+  if #lines == 0 then
+    table.insert(lines, "No results found")
+  end
+
+  fzf.fzf_exec(lines, {
+    prompt = "DD Metrics> ",
+    actions = {
+      ["default"] = function(selected)
+        if selected and selected[1] then
+          fzf.live_grep({
+            search = selected[1],
+            cwd = "app/",
+          })
+        end
+      end,
+    },
+  })
+end, { desc = "Search DD metrics" })
