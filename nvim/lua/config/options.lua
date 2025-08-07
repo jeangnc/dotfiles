673fefa119950efb2f042d0589fba89f9a59fb0c
Jean Gonçalves <jean.gnc@gmail.com>
2025-08-07 08:54:46 -0300

    Fix annoying cwd behavior

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- don't change cwd when find a .git
+vim.g.root_spec = { "cwd" }
+
 vim.g.mapleader = "-"
 vim.g.maplocalleader = "\\"
 vim.opt.signcolumn = "yes:2"
