11a4debd18d539e31b1acae42df37be2bf13dbac
Jean Gonçalves <jean.gnc@gmail.com>
2025-08-23 15:53:04 -0600

    Fix broken catppuccin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Apply Catppuccin compatibility fix
+require("config.catppuccin-fix")
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
