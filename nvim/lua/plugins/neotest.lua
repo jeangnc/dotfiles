e607c51a20d23fcc20f21fc9ad34d531d38d6332
Jean Gonçalves <jean.gnc@gmail.com>
2025-08-29 06:39:26 -0300

    Add neotest-go

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -3,6 +3,7 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "olimorris/neotest-rspec",
+      "nvim-neotest/neotest-go",
       "zidhuss/neotest-minitest",
     },
     opts = {
@@ -24,23 +25,6 @@ return {
           end,
           results_path = "tmp/rspec.output",
           formatter = "json",
-        },
-        ["neotest-minitest"] = {
-          test_cmd = function()
-            return vim.tbl_flatten({
-              "docker-compose",
-              "exec",
-              "web",
-              "bundle",
-              "exec",
-              "rails",
-              "test",
-            })
-          end,
-          transform_spec_path = function(path)
-            local prefix = require("neotest-minitest").root(path)
-            return string.sub(path, string.len(prefix) + 2, -1)
-          end,
         },
       },
     },
