564cadc8c35a6114f5669c71dfecb012c84a44e2
Jean Gonçalves <jean.gnc@gmail.com>
2025-09-01 12:04:57 -0300

    Autoindent after edit

--- nvim/.config/nvim/ftplugin/norg.lua
+++ nvim/.config/nvim/ftplugin/norg.lua
@@ -24,18 +24,23 @@ vim.keymap.set({ "n", "i", "o" }, "<Tab>", function()
   vim.cmd('lua require("neorg.modules.core.itero.module").public.next_iteration_cr()')
 end, { desc = "", buffer = true })
 
+-- Auto-indent on save and when leaving insert mode for .norg files
+vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
+  pattern = "*.norg",
+  callback = function()
+    local view = vim.fn.winsaveview()
+    vim.cmd("silent! normal! gg=G")
+    vim.fn.winrestview(view)
+  end,
+})
+
 -- LSP configuration for neorg files
 vim.api.nvim_create_autocmd("LspAttach", {
-  buffer = 0, -- Only for current buffer
+  pattern = "*.norg",
   callback = function(args)
     local bufnr = args.buf
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if not client then
-      return
-    end
-
-    -- Only apply to norg files
-    if vim.bo[bufnr].filetype ~= "norg" then
       return
     end
 
