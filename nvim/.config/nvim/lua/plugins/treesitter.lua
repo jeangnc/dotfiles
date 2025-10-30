dd01a9c9acab357a13b667aed41b4bd2f19be858
Jean Gonçalves <jean.gnc@gmail.com>
2025-10-30 10:20:02 -0300

    Fix treesitter not enabled in neorg

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,14 +2,28 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     commit = "main",
-    opts = {
-      incremental_selection = {
+    opts = function(_, opts)
+      opts.incremental_selection = {
         enable = false,
-      },
+      }
       -- Disable auto-install to prevent corruption issues
-      auto_install = false,
+      opts.auto_install = false
       -- Skip problematic parsers
-      ignore_install = { "jsonc" },
-    },
+      opts.ignore_install = { "jsonc" }
+      -- Register norg parser (installed via luarocks.nvim) so LazyVim recognizes it
+      opts.ensure_installed = {
+        "norg",
+      }
+
+      -- Manually start treesitter for norg files (luarocks-installed parser)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "norg",
+        callback = function(event)
+          pcall(vim.treesitter.start, event.buf)
+        end,
+      })
+
+      return opts
+    end,
   },
 }
