f18fe9e82f0e5a154090df75c0d54928a7b25c5a
Jean Gonçalves <jean.gnc@gmail.com>
2026-01-27 13:45:38 -0300

    Adds gworktree aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,25 @@ alias gps='git push origin "$(git_current_branch)"'
 alias gpl='git pull origin "$(git_current_branch)"'
 alias gbd='function _gbd() { git branch -D "$1" && git push origin --delete "$1"; }; _gbd'
 
+# git worktree
+alias gwt='function _gwt() {
+  local branch="$1"
+  local repo=$(basename "$(git rev-parse --show-toplevel)")
+  local path="/tmp/git-worktrees/$repo/$branch"
+  if [[ -d "$path" ]]; then
+    echo "Worktree already exists at $path"
+  else
+    git worktree add "$path" "$branch"
+  fi
+}; _gwt'
+
+alias gwte='function _gwte() {
+  local branch="$1"
+  local repo=$(basename "$(git rev-parse --show-toplevel)")
+  gwt "$branch"
+  $EDITOR "/tmp/git-worktrees/$repo/$branch"
+}; _gwte'
+
 # "git update"
 alias gu='git checkout -q main && git pull && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
 
