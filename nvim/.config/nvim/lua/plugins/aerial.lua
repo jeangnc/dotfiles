d7b366aef7ae159b50fb0ce11b14379436a781a0
Jean Gonçalves <jean.gnc@gmail.com>
2026-02-09 09:52:40 -0300

    Changes aerial settings

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -5,9 +5,25 @@ return {
       { "<localleader><tab>", "<cmd>AerialToggle<cr>", desc = "Toggle Outline/ToC" },
     },
     opts = {
+      -- Priority list of preferred backends for aerial.
+      -- This can be a filetype map (see :help aerial-filetype-map)
+      backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
       layout = {
+        min_width = 0.3,
         default_direction = "left",
       },
+
+      -- Jump to symbol in source window when the cursor moves
+      autojump = true,
+
+      -- Use symbol tree for folding. Set to true or false to enable/disable
+      -- Set to "auto" to manage folds if your previous foldmethod was 'manual'
+      -- This can be a filetype map (see :help aerial-filetype-map)
+      -- manage_folds = true,
+
+      -- When you fold code with za, zo, or zc, update the aerial tree as well.
+      -- Only works when manage_folds = true
+      -- link_folds_to_tree = true,
     },
   },
 }
